848b7a46884a7caa3a45edc5df917b22e7a9750e
liudaojie <liudaojie@yy.com>
2021-05-08 18:39:40 +0800

    feat: 去掉敏感信息

--- test/jest/index.test.js
+++ test/jest/index.test.js
@@ -2,13 +2,13 @@ const { Fetcher } = require('../../')
 test('get usage test', async () => {
   const fetcher = new Fetcher({ timeout: 2000 })
 
-  const rs = await fetcher.get('https://www.yy.com/yyweb/module/data/header')
+  const rs = await fetcher.get('https://9u9ntpb8xp.api.quickmocker.com/getter-test')
   expect(rs).not.toEqual({})
 })
 
 test('post usage test', async () => {
   const fetcher = new Fetcher({ timeout: 2000 })
 
-  const rs = await fetcher.post('https://www.yy.com/yyweb/module/data/header')
+  const rs = await fetcher.post('https://9u9ntpb8xp.api.quickmocker.com/getter-test')
   expect(rs).not.toEqual({})
 })
